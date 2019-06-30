3db094c06a7266fbba39cc261ba6e687aae82502
GL553 <GL553@LAPTOP-TR1AEHJH>
2019-06-30 21:19:16 +0200

    Ultima actualizacion

--- github.com/P0M3L0/HipermercadoOnline.git/Gestion_Supermercado/src/clases_basicas/User.java
+++ github.com/P0M3L0/HipermercadoOnline.git/Gestion_Supermercado/src/clases_basicas/User.java
@@ -13,6 +13,18 @@ public class User {
 	String password;
 	//Getter & Setters //++
 	
+	public User (String nombre, String apellido_1, String apellido_2, int edad, boolean es_admin, String nif, String num_telefono, String User_name, String password ) {
+		this.nombre = nombre;
+		this.apellido_1 = apellido_1;
+		this.apellido_2 = apellido_2;
+		this.edad = edad;
+		this.es_admin = es_admin;
+		this.nif = nif;
+		this.num_telefono = num_telefono;
+		this.User_name = User_name;
+		this.password = password;
+	}
+	
 	public String getNombre() {
 		return nombre;
 	}
@@ -67,6 +79,7 @@ public class User {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
 	
 	
 	
